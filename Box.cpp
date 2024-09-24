42e92cf776ab947f87dbdb7a44d259b614e7d280
addictedK1 <kaushalparmar018@gmail.com>
2024-09-24 20:54:30 +0530

    boxPatternCodeDone

--- Box.cpp
+++ Box.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
 
+void BoxPattern()  {
+    int r;
+    cout<<"enter row : ";
+    cin>>r;
+
+    for(int i=0; i<r; i++)  {
+        for(int j=0; j<r; j++)  {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+}
+
+int main()  {
+    
+    BoxPattern();
+    return 0;
+}
